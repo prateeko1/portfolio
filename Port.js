ab898e53d49c44dcc70efb0c200809094c1efb23
Prateek <prateekprajapati2001@gmail.com>
2023-04-08 22:09:58 +0530

    reverting local changes

--- Port.js
+++ Port.js
@@ -7,6 +7,5 @@ var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
-  console.log(myInput);
 })
 
