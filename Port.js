94be61f777ef4caf064a8b0c9354191c146c6dc5
Prateek <prateekprajapati2001@gmail.com>
2023-04-08 21:54:54 +0530

    First Commit

---
+++ Port.js
@@ -0,0 +1,11 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+    if(loader) loader.style.display = "none";
+})
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
